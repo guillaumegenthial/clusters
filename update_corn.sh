995e2fdb0b3b36f30e9a148a29321f93d8a039cc
Guillaume Genthial <genthial.guillaume@gmail.com>
2017-02-23 23:47:49 -0800

    removed pers files

--- update_corn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -rtv --exclude 'data/ntuple_v3_2000k.npy' . genthial@corn.stanford.edu:~/private/clusters
--- update_slac.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -rtv --exclude-from='.rsyncignore' . genthial@atlint03.slac.stanford.edu:~/private/clusters
